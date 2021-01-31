3b83e6326508e58e0a8d4d85ee510c0682c83c54
Angel Garcia <angelgarcia112198@gmail.com>
2021-01-30 19:02:18 -0800

    Added page numbers and concept splitting table by 10 entries

--- OneDrive/Desktop/ticketappReact/ticketapp/src/components/Requests.jsx
+++ OneDrive/Desktop/ticketappReact/ticketapp/src/components/Requests.jsx
@@ -7,18 +7,22 @@ class Request extends Component{
         super(props);
         this.state = {
             tableHeader: ['Issue', 'Date', 'User', 'Priority'],
-            requests: this.props.requests,
-            page : 1
+            requests: [],
+            page: 1,
+            pageList: [],
         }
     }
 
     componentDidMount() {
       // Uncomment when requests are designed nicely
-      //this.splitRequestsToPages();
+      this.splitRequestsToPages();
     }
 
-    render () {
-        return (
+    render() {
+        var noRequests = <div className='no-reqs'>There are no tickets!</div>;
+
+        return !this.props.requests.length ? noRequests : (
+          <div>
             <table className='requests'>
               <thead>
                 <tr>{this.state.tableHeader.map(item => (
@@ -28,25 +32,55 @@ class Request extends Component{
               <tbody>
                 {this.state.requests.map((item, idx, array) => (
                   <tr key={item + idx}>
-                  {[<td key={'Issue' + idx}>{array[array.length - 1 - idx].Issue}</td>,
-                    <td key={'Date' + idx}>{array[array.length - 1 - idx].Date}</td>,
-                    <td key={'User' + idx}>{array[array.length - 1 - idx].User}</td>,
-                    <td key={'Priority' + idx}>{array[array.length - 1 - idx].Priority}</td>]}</tr>
+                    {[<td key={'Issue' + idx}>{array[array.length - 1 - idx].Issue}</td>,
+                      <td key={'Date' + idx}>{array[array.length - 1 - idx].Date}</td>,
+                      <td key={'User' + idx}>{array[array.length - 1 - idx].User}</td>,
+                      <td key={'Priority' + idx}>{array[array.length - 1 - idx].Priority}</td>]}
+                  </tr>
                 ))}
-              </tbody>
+              </tbody>           
             </table>
+            <div>
+              <ul className='req-pages'>
+                {this.state.pageList.map((item, idx) => {
+                  return <button className='req-pages-idx' key={idx}>{item}</button>;
+                })}
+              </ul>
+            </div>
+          </div>
         );
     }
 
     // Displays 10 items a page within the requests list
-    splitRequestsToPages () {
+    splitRequestsToPages() {
       var reqs = [];
 
       for (var i = (10 * (this.state.page - 1)); i < (10 * this.state.page) && this.props.requests[i]; i ++)
         reqs.push(this.props.requests[i]);
         
       //console.log(reqs);
-      this.setState({requests: reqs})
+      this.setState({requests: reqs}, () => {
+        this.formatPages();
+      })
+    }
+
+    // Formats page indeces on the bottom to scroll through the requests list
+    // Allows for prev 2 indeces and next 2 indeces to be displayed;
+    formatPages() {
+      const pagesMaxSize = 6;
+      const firstPageNumber = 1;
+      const maxPage = Math.ceil(this.state.requests.length / 10);
+      var pages = [];
+      var i = this.state.page > 2 ? this.state.page - 2 : firstPageNumber;
+
+      for (; i <= maxPage && pages.length < pagesMaxSize; i ++) {
+        pages.push(i);
+      }
+
+      if (pages[pages.length] !== maxPage && pages.length === pagesMaxSize)
+        pages[pages.length - 1] = maxPage;
+
+      this.setState({pageList: pages})
     }
 }
 
